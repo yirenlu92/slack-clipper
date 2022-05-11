79c609904382ff18ac9c795beeeb792ac0ae9348
Jainam Chirag Shah <jcshah98@gmail.com>
2022-05-11 14:34:12 -0400

    Get conversation thread

--- slack-clipper-backend/app.py
+++ slack-clipper-backend/app.py
@@ -1,52 +1,35 @@
 import os
-# Use the package we installed
 from slack_bolt import App
 
-# Initializes your app with your bot token and signing secret
 app = App(
     token=os.environ.get("SLACK_BOT_TOKEN"),
     signing_secret=os.environ.get("SLACK_SIGNING_SECRET")
 )
 
-# Add functionality here
-# @app.event("app_home_opened") etc
+def get_conversation_thread(client, channel, ts):
+    replies = client.conversations_replies(channel=channel, ts=ts)
+    messages = list()
+    for message in replies["messages"]:
+        messages.append({
+            "text": message["text"],
+            "user": message["user"]
+            })
+    return messages
 
-# The open_modal shortcut listens to a shortcut with the callback_id "clip_markdown"
 @app.shortcut("clip_markdown")
 def clip_markdown(ack, shortcut, client):
-    # Acknowledge the shortcut request
     ack()
-    # Call the views_open method using the built-in WebClient
-    client.views_open(
-        trigger_id=shortcut["trigger_id"],
-        # A simple view payload for a modal
-        view={
-            "type": "modal",
-            "title": {"type": "plain_text", "text": "My App"},
-            "close": {"type": "plain_text", "text": "Close"},
-            "blocks": [
-                {
-                    "type": "section",
-                    "text": {
-                        "type": "mrkdwn",
-                        "text": "About the simplest modal you could conceive of :smile:\n\nMaybe <https://api.slack.com/reference/block-kit/interactive-components|*make the modal interactive*> or <https://api.slack.com/surfaces/modals/using#modifying|*learn more advanced modal use cases*>."
-                    }
-                },
-                {
-                    "type": "context",
-                    "elements": [
-                        {
-                            "type": "mrkdwn",
-                            "text": "Psssst this modal was designed using <https://api.slack.com/tools/block-kit-builder|*Block Kit Builder*>"
-                        }
-                    ]
-                }
-            ]
-        }
-    )
 
+    channel = shortcut["channel"]["id"]
+    ts = shortcut["message"]["ts"]
+    title = shortcut["message"]["text"]
+
+    print(f"[CLIP] channel={channel} ts={ts} thread-title='{title}'")
+    
+    conversation_thread = get_conversation_thread(client, channel, ts)
+    print("conversation_thread", conversation_thread)
 
 
 # Start your app
 if __name__ == "__main__":
-    app.start(port=int(os.environ.get("PORT", 3000)))
+    app.start(port=int(os.environ.get("PORT", 5000)))
