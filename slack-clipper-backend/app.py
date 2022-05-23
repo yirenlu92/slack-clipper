ed9fe0c714a896e18038d6f595c7531350a05aa5
Andre Dubovskiy <dubovski@Dogwater.localdomain>
2022-05-23 16:56:05 -0400

    added ephemeral reply-in-channel

--- slack-clipper-backend/app.py
+++ slack-clipper-backend/app.py
@@ -18,43 +18,56 @@ firebase_admin.initialize_app(cred, {
 db = firestore.client()
 bucket = storage.bucket()
 
+
 def get_conversation_thread(client, channel, ts):
     replies = client.conversations_replies(channel=channel, ts=ts)
     messages = list()
     users = {}
     for message in replies["messages"]:
+        print(message)
         user_id = message["user"]
         if user_id not in users:
             user_info = client.users_info(user=user_id)
             users[user_id] = {
                 "name": user_info["user"]["name"],
                 "profile_picture": user_info["user"]["profile"]["image_24"]
-                }
-        
+            }
+
         messages.append({
             "text": message["text"],
             "name": users[user_id]["name"],
-            "profile_picture": users[user_id]["profile_picture"]
-            })
-        
+            "profile_picture": users[user_id]["profile_picture"],
+            "files": message["files"]
+        })
+
     return messages
 
-def make_markdown_message(name, profile_picture, text):
-    return f"""
+
+def make_markdown_message(name, profile_picture, text, files):
+    returnString = f"""
 <div style="-webkit-column-count: 2; -moz-column-count: 2; column-count: 2;">
     <div style="display: inline-block;">
         <img src="{profile_picture}" alt="{name}">
     </div>
     <div style="display: inline-block;">
-        <strong>{name}</strong>
+            <strong>{name}</strong>   
+        </div>
     </div>
-</div>
+    {text}
+            
+"""
+    for file in files:
+        returnString += "\n"
+        returnString += f"""<img src="{file["permalink"]}">"""
 
-{text}
-        
-        """
+    returnString+="</div>"
+
+    return returnString
+
 
 def get_markdown_text(conversation_thread):
+
+
     markdown_text = ""
 
     if len(conversation_thread) > 0:
@@ -62,34 +75,45 @@ def get_markdown_text(conversation_thread):
         markdown_text += make_markdown_message(
             message["name"],
             message["profile_picture"],
-            message["text"]
-            )
-    
+            message["text"],
+            message["files"]
+        )
+
     markdown_text += """
----
-    """
+    ---
+        """
 
     for message in conversation_thread[1:]:
         markdown_text += make_markdown_message(
             message["name"],
             message["profile_picture"],
             message["text"]
-            )
-    
+        )
+
     return markdown_text
 
 
-def upload_markdown_file_to_firebase(markdown_text):
+def upload_markdown_file_to_firebase(markdown_text, userid, channelid):
     print("Uploading to Firebase...")
     filename = f"{uuid4()}.md"
     with open(filename, "w") as f:
         f.write(markdown_text)
-    
+
     blob = bucket.blob(filename)
     blob.metadata = {"firebaseStorageDownloadTokens": uuid4()}
-    outfile=os.path.abspath(filename)
+    outfile = os.path.abspath(filename)
     blob.upload_from_filename(outfile)
-    
+
+    blob.make_public()
+    print(blob.public_url)
+    app.client.chat_postEphemeral(
+        user=userid,
+        channel=channelid,
+        text=blob.public_url
+        # You could also use a blocks[] array to send richer content
+    )
+    print("tried to send a message")
+
     os.remove(filename)
     print(f"File uploaded successfully: {filename}")
 
@@ -99,14 +123,15 @@ def clip_markdown(ack, shortcut, client):
     ack()
 
     channel = shortcut["channel"]["id"]
+    userid = shortcut["user"]["id"]
     ts = shortcut["message"]["ts"]
     title = shortcut["message"]["text"]
 
     print(f"[CLIP] channel={channel} ts={ts} thread-title='{title}'")
-    
+
     conversation_thread = get_conversation_thread(client, channel, ts)
     markdown_text = get_markdown_text(conversation_thread)
-    upload_markdown_file_to_firebase(markdown_text)
+    upload_markdown_file_to_firebase(markdown_text, userid, channel)
 
 
 # Start your app
